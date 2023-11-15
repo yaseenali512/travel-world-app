3ec5fd3ce1f8c8e7f7557566ef0f9b1e345284b7
yaseenali512 <yaseenali.bscssef20@iba-suk.edu.pk>
2023-11-15 01:59:55 -0600

    first commit

---
+++ travelworld/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="app" element={<AppLayout />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ travelworld/src/components/AppNav.jsx
@@ -0,0 +1,6 @@
+import styles from "./AppNav.module.css";
+function AppNav() {
+  return <div className={styles.nav}>App Nav</div>;
+}
+
+export default AppNav;
